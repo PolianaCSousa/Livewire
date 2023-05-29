8ab4066ba14598e70136ce247f7e9dff96278d8e
Poliana <polianasamonte@gmail.com>
2023-05-29 10:51:40 -0300

    Terminar implementação da função de editar os nomes

--- app/Http/Livewire/ListaBeer.php
+++ app/Http/Livewire/ListaBeer.php
@@ -6,8 +6,43 @@ use Livewire\Component;
 
 class ListaBeer extends Component
 {
+
+    //exemplo do site do livewire
+    public $count = 0;
+
+    public function incrementa()
+    {
+        $this->count++;
+    }
+
+    //codigo beer and code
+    /*
+    O Livewire atribuirá parâmetros automaticamente às propriedades públicas correspondentes. Portanto, se eu passar um atributo de nome item, ou lista, os valores passados (via componente ou via classe) serão atualizados pra view.
+    */ 
+    public $item;
+    public $lista = ['Frank', 'Icaro', 'Lucas', 'Danilo', 'Lamarques', 'Tom'];
+
     public function render()
     {
         return view('livewire.lista-beer');
+    }
+
+    public function add(){
+        array_push($this->lista, $this->item);
+        
+        $this->item = '';
+    }
+
+    public function resetList(){
+        unset($this->lista);
+        $this->lista = [];
+    }
+
+    public function delete($key){
+        unset($this->lista[$key]);
+    }
+
+    public function edit($key){
+        
     }
 }
--- resources/views/livewire/lista-beer.blade.php
+++ resources/views/livewire/lista-beer.blade.php
@@ -1,3 +1,37 @@
-<div>
-    {{-- The best athlete wants his opponent at his best. --}}
+<div class="container">
+  <div class="row">
+    <div class="col-6"> 
+      Clique aqui
+      <button wire:click="incrementa">+</button>
+    </div>
+    <div class="col-6">
+      <p>{{$count}}</p>
+    </div>
+  </div>
+
+  <hr/>
+
+  <div class="row">
+    <div class="col-auto">
+      <input class="mb-2" type="text" wire:model="item">
+      <button wire:click="add" class="btn btn-primary">Adicionar</button>
+      <button wire:click="resetList" class="btn btn-secondary">Limpar Lista</button>
+      <ul>
+        @foreach($lista as $key => $nome)
+          <div class="row">
+            <div class="col-auto mb-3">
+            <li>{{$nome}}</li>
+            </div>
+            <div class="col-auto">
+              <button class="btn btn-success" wire:click="edit({{$key}})">Editar</button>
+            </div>
+            <div class="col-auto">
+              <button class="btn btn-danger" wire:click="delete({{$key}})">Deletar</button>
+            </div>    
+          </div>
+        @endforeach
+      </ul>
+    </div>
+  </div>
 </div>
+
